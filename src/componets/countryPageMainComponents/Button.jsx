d80dd72528c4e33e3b597193f2f482b16f80f5d3
Fernando Andres Poblete <andrespbt14@hotmail.com>
2023-03-14 10:26:36 -0300

    feat: project finished

--- src/componets/countryPageMainComponents/Button.jsx
+++ src/componets/countryPageMainComponents/Button.jsx
@@ -4,10 +4,10 @@ export const Button = ({ classes, onClick, icon, text }) => {
   const { button, span } = classes;
   return (
     <button
-      className={`${button} border-[1px] border-darkGray/10 dark:bg-darkBlue dark:text-white rounded-sm h-[35px] relative shadow-xl bg-white`}
+      className={`${button} border-[1px] border-darkGray/10 dark:bg-darkBlue dark:text-white rounded-sm h-[50px] relative shadow-xl bg-white`}
       onClick={onClick}>
       {icon && icon}
-      <span className={`${span} text-base`}>{text}</span>
+      <span className={`${span} text-base`}>{text?.length > 10 ? text.slice(0, 10).concat('...') : text}</span>
     </button>
   );
 };
