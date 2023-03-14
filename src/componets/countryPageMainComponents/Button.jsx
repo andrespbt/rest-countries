51f21bc224c56faff777d7c2016f89500f9795a0
Fernando Andres Poblete <andrespbt14@hotmail.com>
2023-03-14 12:19:33 -0300

    feat: full names on hover country button added

--- src/componets/countryPageMainComponents/Button.jsx
+++ src/componets/countryPageMainComponents/Button.jsx
@@ -1,11 +1,19 @@
 export const Button = ({ classes, onClick, icon, text }) => {
+  const tooltip = text?.length > 10;
   const { button, span } = classes;
   return (
-    <button
-      className={`${button} border-[1px] border-darkGray/10 dark:bg-darkBlue dark:text-white rounded-sm h-[50px] relative shadow-xl bg-white`}
-      onClick={onClick}>
-      {icon && icon}
-      <span className={`${span} text-base`}>{text?.length > 10 ? text.slice(0, 10).concat('...') : text}</span>
-    </button>
+    <div className="relative">
+      <button
+        className={`${button} peer border-[1px] border-darkGray/10 dark:bg-darkBlue dark:text-white rounded-sm h-[50px] relative shadow-xl bg-white`}
+        onClick={onClick}>
+        {icon && icon}
+        <span className={`${span} text-base`}>{text?.length > 10 ? text.slice(0, 10).concat('...') : text}</span>
+      </button>
+      {tooltip && (
+        <span className="hidden absolute peer-hover:inline top-[-25px] border-2 p-2 text-sm bg-[black] text-white rounded-md z-10 w-full text-center">
+          {text}
+        </span>
+      )}
+    </div>
   );
 };
